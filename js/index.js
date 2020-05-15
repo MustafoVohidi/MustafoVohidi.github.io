2327b52230c1f4c2a8730dd11d09bd577d72167f
MustafoVohidi <48031070+MustafoVohidi@users.noreply.github.com>
2020-05-16 00:54:15 +0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,16 +1,18 @@
-var slider = document.getElementById("myRange");
-var levone = document.getElementById("levone");
-var levtwo = document.getElementById("levtwo");
-var levthree = document.getElementById("levthree");
+
+
+let slider = document.getElementById("myRange");
+let levone = document.getElementById("levone");
+let levtwo = document.getElementById("levtwo");
+let levthree = document.getElementById("levthree");
 
 slider.oninput = function () {
-    if (this.value <= 30) {
+    if (this.value >= -30) {
         levone.classList.remove("hidden");
         levtwo.classList.add("hidden");
         levthree.classList.add("hidden");
      
     }
-    else if (this.value > 30 && this.value <= 60) {
+    else if (this.value < -30 && this.value >= -60) {
         levone.classList.add("hidden");
         levtwo.classList.remove("hidden");
         levthree.classList.add("hidden");
@@ -26,11 +28,10 @@ slider.oninput = function () {
 };
 
 
-
 const slideWrapper = document.querySelector('.slide-wrapper');
-const nav_0 = document.querySelector('.nav__0');
-const nav_1 = document.querySelector('.nav__1');
-const nav_2 = document.querySelector('.nav__2');
+const nav_0 = document.querySelector('.nav0');
+const nav_1 = document.querySelector('.nav1');
+const nav_2 = document.querySelector('.nav2');
 let touchStartY;
 let touchEndY;
 let currentSlide = 0;
@@ -40,7 +41,7 @@ const update = function(currentSlide) {
 	nav_1.style.backgroundColor = 'white';
 	nav_2.style.backgroundColor = 'white';
   	slideWrapper.style.transform = `translateY(-${currentSlide*768}px)`;
-	document.querySelector('.nav__' + currentSlide).style.backgroundColor = '#f78b1f';
+	document.querySelector('.nav' + currentSlide).style.backgroundColor = '#f78b1f';
 
 	 if (currentSlide == 2) {
 	 	document.getElementById('scroll-down').style.opacity = 0;
